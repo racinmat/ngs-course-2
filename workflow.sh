8f03f47ab7c69535b5a0678c93d4b6399330e80a
Matej Racinsky <racinsky.matej@seznam.cz>
2023-11-26 14:25:08 +0100

    very fancy tee magic

--- workflow.sh
+++ workflow.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
 
-FILE=data/luscinia_vars.vcf.gz 
-<$FILE zcat | grep -v "^#" | awk 'match($1, /^chr[1,Z]$/)' | cut -f1-6 | gzip > data/col1-6.gz
-<$FILE zcat | grep -v "^#" | awk 'match($1, /^chr[1,Z]$/) {print $8}' | egrep -o 'DP=[^;]*' | sed 's/DP=//' | gzip > data/dp_val.gz
-<$FILE zcat | grep -v "^#" | awk 'match($1, /^chr[1,Z]$/) {print $8}' | awk '{if($0 ~ /INDEL/) print "INDEL"; else print "SNP"}' | gzip > data/indel.gz
-paste <(zcat data/col1-6.gz) <(zcat data/dp_val.gz) <(zcat data/indel.gz) | gzip > data/result.csv.gz
+INPUT=data/luscinia_vars.vcf.gz
 
+first_6() {
+    # obtaining first 6 columns
+#    echo "$1"
+    cut -f1-6 |
+    gzip > "$1"
+}
+
+get_dp() {
+    # obtaining dp stuff
+    echo "$1"
+    cut -f8 |
+    egrep -o 'DP=[^;]*' |
+    sed 's/DP=//' |
+    gzip > "$1"
+}
+
+get_indel() {
+    # indel stuff
+#    echo "$1"
+    awk '{if($8 ~ /INDEL/) print "INDEL"; else print "SNP"}' |
+    gzip > "$1"
+}
+
+#filtering the data
+<$INPUT zcat |
+  grep -v "^#" |
+  awk 'match($1, /^chr[1,Z]$/)' |
+  tee \
+    >(first_6 data/col1-6.tsv.gz) \
+    >(get_dp data/dp_val.tsv.gz) \
+    >(get_indel data/indel.tsv.gz) \
+  > /dev/null
+
+# concatenating them thogether
+paste <(zcat data/col1-6.tsv.gz) <(zcat data/dp_val.tsv.gz) <(zcat data/indel.tsv.gz) | \
+  gzip \
+> data/chr1_chrZ_plotting.tsv.gz
+
+# checking if everything is ok
+sleep 1 # for some reason data is not flushed immediately
+wc -l data/*
+find data/* -type f -print0 | xargs -0 -I{} sh -c "zcat {} | wc -l"
